trestletech/rgl

commit 65fa36f46fbec0b0174c484304cabf0118e4d3fd

First pass at fix for writeWebGL problems.  The NULL device will still attempt some OpenGL calls which may lead to errors; will track those down before release.
git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1137 79a1b648-8b68-254f-b20b-c0b50f5137bf

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: rgl
-Version: 0.94.1135
+Version: 0.94.1137
 Title: 3D visualization device system (OpenGL)
 Author: Daniel Adler <dadler@uni-goettingen.de>, Duncan Murdoch <murdoch@stats.uwo.ca>, and others (see README)
 Maintainer: Duncan Murdoch <murdoch@stats.uwo.ca>

src/geom.cpp
@@ -166,3 +166,28 @@ void Frustum::enclose(float sphere_radius, float fovangle, int width, int height
   bottom = -hheight;
   top    =  hheight;
 }
+
+Matrix4x4 Frustum::getMatrix()
+{
+  double data[16];
+  memset(data, 0, sizeof(data));
+  if (ortho) {
+    data[0] = 2/(right - left);
+    data[5] = 2/(top - bottom);
+    data[10] = -2/(zfar - znear);
+    data[12] = -(right + left)/(right - left);
+    data[13] = -(top + bottom)/(top - bottom);
+    data[14] = -(zfar + znear)/(zfar - znear);
+    data[15] = 1;
+  } else {
+    data[0] = 2*znear/(right - left);
+    data[5] = 2*znear/(top - bottom);
+    data[8] = (right + left)/(right - left);
+    data[9] = (top + bottom)/(top - bottom);
+    data[10] = -(zfar + znear)/(zfar - znear);
+    data[11] = -1;
+    data[14] = -2*zfar*znear/(zfar - znear);
+  }
+  return Matrix4x4(data);
+}
+   

src/subscene.cpp
@@ -15,7 +15,8 @@ using namespace rgl;
 Subscene::Subscene(Embedding in_viewport, Embedding in_projection, Embedding in_model,
                    bool in_ignoreExtent)
  : SceneNode(SUBSCENE), parent(NULL), do_viewport(in_viewport), do_projection(in_projection),
-   do_model(in_model), viewport(0.,0.,1.,1.),Zrow(), Wrow(), ignoreExtent(in_ignoreExtent)
+   do_model(in_model), viewport(0.,0.,1.,1.),Zrow(), Wrow(),
+   pviewport(0,0,1024,1024), ignoreExtent(in_ignoreExtent)
 {
   userviewpoint = NULL;
   modelviewpoint = NULL;
@@ -23,14 +24,8 @@ Subscene::Subscene(Embedding in_viewport, Embedding in_projection, Embedding in_
   background = NULL;
   bboxChanges = false;
   data_bbox.invalidate();
-  
-  for (int i=0; i<4; i++) {
-    pviewport[i] = i < 2 ? 0 : 1024;
-    for (int j=0;j<4;j++) {
-      modelMatrix[4*i + j] = i == j;
-      projMatrix[4*i + j] = i == j;
-    }
-  }  
+  modelMatrix.setIdentity();
+  projMatrix.setIdentity(); 
 }
 
 Subscene::~Subscene() 
@@ -241,8 +236,8 @@ Subscene* Subscene::whichSubscene(int mouseX, int mouseY)
   for (std::vector<Subscene*>::iterator i = subscenes.begin(); i != subscenes.end() ; ++ i ) {
     result = (sub = (*i)->whichSubscene(mouseX, mouseY)) ? sub : result;
   }  
-  if (!result && pviewport[0] <= mouseX && mouseX < pviewport[0] + pviewport[2] 
-              && pviewport[1] <= mouseY && mouseY < pviewport[1] + pviewport[3])
+  if (!result && pviewport.x <= mouseX && mouseX < pviewport.x + pviewport.width 
+              && pviewport.y <= mouseY && mouseY < pviewport.y + pviewport.height)
     result = this;
   return result;
 }
@@ -493,7 +488,7 @@ ModelViewpoint* Subscene::getModelViewpoint()
 void Subscene::render(RenderContext* renderContext)
 {
   GLdouble saveprojection[16];
-  GLint saveviewport[4] = {0,0,0,0};
+  Rect2 saveviewport(0,0,0,0);
   
   /* FIXME:  the viewpoint object affects both the projection matrix and the
              model matrix.  We need ugly code to use the right one when 
@@ -507,11 +502,10 @@ void Subscene::render(RenderContext* renderContext)
     // SETUP VIEWPORT TRANSFORMATION
     //
     if (parent)  
-      for (int i=0; i<4; i++) saveviewport[i] = parent->pviewport[i];
+      saveviewport = parent->pviewport;
     setupViewport(renderContext);
 
   }
-  glGetIntegerv(GL_VIEWPORT, pviewport);  
   SAVEGLERROR;
   
   if (background) {
@@ -547,19 +541,15 @@ void Subscene::render(RenderContext* renderContext)
   // just use the parent.
   
   if (do_projection > EMBED_INHERIT) {
-    
-    for (int i=0; i<16; i++) saveprojection[i] = projMatrix[i];
+    projMatrix.getData(saveprojection);
     setupProjMatrix(renderContext, total_bsphere);
-  
   }
-  glGetDoublev(GL_PROJECTION_MATRIX, projMatrix);
   
   // Now the model matrix.  Since this depends on both the viewpoint and the model
   // transformations, we don't bother using the parent one, we reconstruct in
   // every subscene.
   
-  setupModelViewMatrix(total_bsphere.center);
-  glGetDoublev(GL_MODELVIEW_MATRIX, modelMatrix);
+  setupModelViewMatrix(renderContext, total_bsphere.center);
   
   setupLights(renderContext);
   
@@ -671,9 +661,9 @@ void Subscene::render(RenderContext* renderContext)
     glLoadMatrixd(saveprojection);
   }
   
-  if (do_viewport > EMBED_INHERIT) 
-    glViewport(saveviewport[0], saveviewport[1], saveviewport[2], saveviewport[3]);
-  
+  if (do_viewport > EMBED_INHERIT)  {
+    glViewport(saveviewport.x, saveviewport.y, saveviewport.width, saveviewport.height);
+  }
 }
 
 void Subscene::calcDataBBox()
@@ -718,12 +708,12 @@ void Subscene::setupViewport(RenderContext* rctx)
     rect.width = rctx->rect.width*viewport.width;
     rect.height = rctx->rect.height*viewport.height;
   } else {
-    rect.x = parent->pviewport[0] + viewport.x*parent->pviewport[2];
-    rect.y = parent->pviewport[1] + viewport.y*parent->pviewport[3];
-    rect.width = parent->pviewport[2]*viewport.width;
-    rect.height = parent->pviewport[3]*viewport.height;
+    rect.x = parent->pviewport.x + viewport.x*parent->pviewport.width;
+    rect.y = parent->pviewport.y + viewport.y*parent->pviewport.height;
+    rect.width = parent->pviewport.width*viewport.width;
+    rect.height = parent->pviewport.height*viewport.height;
   }
-  
+  pviewport = rect;
   glViewport(rect.x, rect.y, rect.width, rect.height);
   glScissor(rect.x, rect.y, rect.width, rect.height);
 }
@@ -731,9 +721,10 @@ void Subscene::setupViewport(RenderContext* rctx)
 void Subscene::setupProjMatrix(RenderContext* rctx, const Sphere& viewSphere)
 {
   glMatrixMode(GL_PROJECTION);
-  if (do_projection == EMBED_REPLACE)
+  if (do_projection == EMBED_REPLACE) {
     glLoadIdentity();
-    
+    projMatrix.setIdentity();
+  }    
   getUserViewpoint()->setupFrustum(rctx, viewSphere);
     
   SAVEGLERROR;    
@@ -744,37 +735,28 @@ void Subscene::setupProjMatrix(RenderContext* rctx, const Sphere& viewSphere)
 // the latter from the modelViewpoint, possibly after applying the same from the parents.
 // We always reconstruct from scratch rather than trying to use the matrix in place.
 
-void Subscene::setupModelViewMatrix(Vertex center)
+void Subscene::setupModelViewMatrix(RenderContext* rctx, Vertex center)
 {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  getUserViewpoint()->setupViewer();
-  setupModelMatrix(center);
+  modelMatrix.setIdentity();
+  getUserViewpoint()->setupViewer(rctx);
+  setupModelMatrix(rctx, center);
   
   SAVEGLERROR;
 }
 
-void Subscene::setupModelMatrix(Vertex center)
+void Subscene::setupModelMatrix(RenderContext* rctx, Vertex center)
 {
   if (do_model < EMBED_REPLACE && parent)
-    parent->setupModelMatrix(center);
+    parent->setupModelMatrix(rctx, center);
     
   if (do_model > EMBED_INHERIT)
-    getModelViewpoint()->setupTransformation(center);
+    getModelViewpoint()->setupTransformation(rctx, center);
   
   SAVEGLERROR;
 }
 
-void Subscene::getModelMatrix(double* modelMatrix, Vertex center)
-{
-  glMatrixMode(GL_MODELVIEW);
-  glPushMatrix();
-  glLoadIdentity();
-  setupModelMatrix(center);
-  glGetDoublev(GL_MODELVIEW_MATRIX, (GLdouble*) modelMatrix);
-  glPopMatrix();
-}
-  
 void Subscene::disableLights(RenderContext* rctx)
 {
     

src/Background.cpp
@@ -139,7 +139,7 @@ void Background::render(RenderContext* renderContext)
     glTranslatef(0.0f,0.0f,-znear);
 
     ModelViewpoint* modelviewpoint = subscene->getModelViewpoint();
-    modelviewpoint->setupOrientation();
+    modelviewpoint->setupOrientation(renderContext);
 
     Shape::render(renderContext);
     glMatrixMode(GL_MODELVIEW); /* just in case... */

src/NULLgui.cpp
@@ -30,19 +30,21 @@ public:
   void show() {};
   void hide() {};
   void bringToTop(int stay) {};
-  void update() {};
+  void update() { if (window) window->paint(); };
   void destroy() { if (window) window->notifyDestroy(); };
   void captureMouse(View* pView) {};
   void releaseMouse() {};
   GLFont* getFont(const char* family, int style, double cex, 
                   bool useFreeType);
+  int getAntialias() { return 1; }
+  int getMaxClipPlanes() { return INT_MAX; }
 
 private:
   int rect[4];
   friend class NULLGUIFactory;
 
 public:
-  bool beginGL() { return 0; };
+  bool beginGL() { return true; };
   void endGL() {};
   void swap() {};
 };

src/Viewpoint.cpp
@@ -98,7 +98,7 @@ float UserViewpoint::getFOV() const
 
 void UserViewpoint::setupFrustum(RenderContext* rctx, const Sphere& viewSphere)
 {
-  frustum.enclose(viewSphere.radius, fov, rctx->subscene->pviewport[2], rctx->subscene->pviewport[3]);
+  frustum.enclose(viewSphere.radius, fov, rctx->subscene->pviewport.width, rctx->subscene->pviewport.height);
   if (!viewerInScene) {
     eye.x = 0.;
     eye.y = 0.;
@@ -130,7 +130,7 @@ void UserViewpoint::setupFrustum(RenderContext* rctx, const Sphere& viewSphere)
   } else {
     glFrustum(frustum.left, frustum.right, frustum.bottom, frustum.top, frustum.znear, frustum.zfar);  
   }
-
+  rctx->subscene->projMatrix.loadData(rctx->subscene->projMatrix*frustum.getMatrix());
 }
 
 Vertex UserViewpoint::getObserver()
@@ -145,24 +145,27 @@ void UserViewpoint::setObserver(bool automatic, Vertex eye)
     this->eye = eye;
 }
 
-void UserViewpoint::setupViewer()
+void UserViewpoint::setupViewer(RenderContext* rctx)
 {
   glTranslatef(-eye.x, -eye.y, -eye.z);
+  rctx->subscene->modelMatrix = rctx->subscene->modelMatrix * Matrix4x4::translationMatrix(-eye.x, -eye.y, -eye.z);
 }
 
-void ModelViewpoint::setupOrientation() const
+void ModelViewpoint::setupOrientation(RenderContext* rctx) const
 {
   glMultMatrixd(mouseMatrix);
   glMultMatrixd(userMatrix);
-
+  rctx->subscene->modelMatrix = rctx->subscene->modelMatrix * mouseMatrix * userMatrix;
 }
 
-void ModelViewpoint::setupTransformation(Vertex center)
+void ModelViewpoint::setupTransformation(RenderContext* rctx, Vertex center)
 {     
   // modelview
-  setupOrientation();
+  setupOrientation(rctx);
   glScaled(scale.x, scale.y, scale.z);
   glTranslatef( -center.x, -center.y, -center.z );
+  rctx->subscene->modelMatrix = rctx->subscene->modelMatrix * Matrix4x4::scaleMatrix(scale.x, scale.y, scale.z)
+                      * Matrix4x4::translationMatrix(-center.x, -center.y, -center.z);
 }
 
 void ModelViewpoint::updateMouseMatrix(Vec3 dragStart, Vec3 dragCurrent)

src/Viewpoint.h
@@ -20,8 +20,8 @@ public:
   PolarCoord& getPosition();
   void        setPosition(const PolarCoord& position);
   void	      clearMouseMatrix();
-  void        setupTransformation(Vertex center);
-  void        setupOrientation() const;
+  void        setupTransformation(RenderContext* rctx, Vertex center);
+  void        setupOrientation(RenderContext* rctx) const;
   bool        isInteractive() const;
   void        updateMouseMatrix(Vertex dragStart,Vertex dragCurrent);
   void	      updateMouseMatrix(PolarCoord newpos);
@@ -59,7 +59,7 @@ public:
   void        setupFrustum(RenderContext* rctx, const Sphere& viewvolumeSphere);
   Vertex      getObserver();
   void	      setObserver(bool automatic, Vertex eye);
-  void	      setupViewer();
+  void	      setupViewer(RenderContext* rctx);
   virtual void getTypeName(char* buffer, int buflen) { strncpy(buffer, "userviewpoint", buflen); };
   Frustum     frustum;
 private:

src/geom.h
@@ -55,6 +55,7 @@ class Frustum {
 public:
   Frustum() : ortho(false) {};
   void enclose(float sphere_radius, float fovangle, int win_width, int win_height);
+  Matrix4x4 getMatrix();
   float left, right, bottom, top, znear, zfar, distance;
   bool ortho;
 };

commit 8001cb2d19fbdfbb1d1c512e836a561cb7a71e8b

Fix bug reported by Dieter Menne, plus new errors introduced in r1137
git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1142 79a1b648-8b68-254f-b20b-c0b50f5137bf

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: rgl
-Version: 0.94.1140
+Version: 0.94.1141
 Title: 3D visualization device system (OpenGL)
 Author: Daniel Adler <dadler@uni-goettingen.de>, Duncan Murdoch <murdoch@stats.uwo.ca>, and others (see README)
 Maintainer: Duncan Murdoch <murdoch@stats.uwo.ca>

src/geom.cpp
@@ -38,8 +38,10 @@ void AABox::operator += (const Vertex& v)
 
 void AABox::operator += (const AABox& aabox)
 {
-  *this += aabox.vmin;
-  *this += aabox.vmax;
+  if (aabox.isValid()) {
+    *this += aabox.vmin;
+    *this += aabox.vmax;
+  }
 }
 
 void AABox::operator += (const Sphere& sphere)

src/subscene.cpp
@@ -505,7 +505,9 @@ void Subscene::render(RenderContext* renderContext)
       saveviewport = parent->pviewport;
     setupViewport(renderContext);
 
-  }
+  } else
+    pviewport = parent->pviewport;
+    
   SAVEGLERROR;
   
   if (background) {
@@ -543,7 +545,8 @@ void Subscene::render(RenderContext* renderContext)
   if (do_projection > EMBED_INHERIT) {
     projMatrix.getData(saveprojection);
     setupProjMatrix(renderContext, total_bsphere);
-  }
+  } else
+    projMatrix = parent->projMatrix;
   
   // Now the model matrix.  Since this depends on both the viewpoint and the model
   // transformations, we don't bother using the parent one, we reconstruct in
@@ -753,6 +756,8 @@ void Subscene::setupModelMatrix(RenderContext* rctx, Vertex center)
     
   if (do_model > EMBED_INHERIT)
     getModelViewpoint()->setupTransformation(rctx, center);
+  else
+    modelMatrix = parent->modelMatrix;
   
   SAVEGLERROR;
 }